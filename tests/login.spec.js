191f2c48bf8117d2442cab76fd74874791425e93
dmitryou <dmitryourkevich@gmail.com>
2025-04-28 02:44:52 -0600

    tests

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,11 +1,12 @@
 import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
-  await page.goto('http://localhost:3000/');
-  await page.getByRole('link', { name: 'Login' }).nth(1).click();
-  await page.getByRole('textbox', { name: 'Email' }).click();
-  await page.getByRole('textbox', { name: 'Email' }).fill('dmitryou@gmail.com');
-  await page.getByRole('textbox', { name: 'Password' }).click();
-  await page.getByRole('textbox', { name: 'Password' }).fill('123qwe');
-  await page.getByRole('button', { name: 'Log In' }).click();
-});
+	await page.goto('http://localhost:3000/');
+	await page.getByRole('link', { name: 'Account' }).click();
+	await page.getByRole('link', { name: 'Login' }).click();
+	await page.getByRole('textbox', { name: 'Email' }).click();
+	await page.getByRole('textbox', { name: 'Email' }).fill('dmitry@gmail.com');
+	await page.getByRole('textbox', { name: 'Email' }).click();
+	await page.getByRole('button', { name: 'Log In' }).click();
+	await page.getByRole('link', { name: 'Home' }).click();
+  });
